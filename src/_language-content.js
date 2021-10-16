e42850e2a518a34dd8b20a597d5baf8c12853335
StasBeep <stas-stas.stanislav@mail.ru>
2021-10-16 21:49:52 +0300

    Положение блоков и добавление контента 1. Добавление файлов для анимации и переменных 2. Добавление файла для хранения контента на русском и английском языке 3. Подключение этих файлов 4. Добавление svg иконок 5. Стилизация кнопки переключения языка

---
+++ src/_language-content.js
@@ -0,0 +1,15 @@
+const contentRu = {
+        elMenu1: "Донат",
+        elMenu2: "Блог",
+        elMenu3: "Контакты",
+        elMenu4: "О нас"
+    };
+
+const contentEn = {
+        elMenu1: "Donat",
+        elMenu2: "The blog",
+        elMenu3: "Contacts",
+        elMenu4: "About us"
+    };
+
+export {contentRu, contentEn};
