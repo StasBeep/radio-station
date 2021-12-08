76814adedcbb7fd5d6cbc46072d20e1a178d7f09
StasBeep <stas-stas.stanislav@mail.ru>
2021-12-08 23:02:01 +0300

    обновлённый адаптив меню на js страницы about us

--- src/_ab-adaptive.js
+++ src/_ab-adaptive.js
@@ -24,12 +24,18 @@ checkEl.addEventListener('click', () => {
 });
 
 export function sizeMenu() {
-    if (window.innerWidth <= 768 && window.innerWidth > 600) {
+    if (window.innerWidth <= 768 && window.innerWidth > 725) {
         styleChange768(toggle);
-    } else if (window.innerWidth <= 600 && window.innerWidth > 480) {
+    } else if (window.innerWidth <= 725 && window.innerWidth > 600) {
+        styleChange725(toggle);
+    } else if (window.innerWidth <= 600 && window.innerWidth > 560) {
         styleChange600(toggle);
-    } else if (window.innerWidth <= 480 && window.innerWidth > 375) {
+    } else if (window.innerWidth <= 560 && window.innerWidth > 480) {
+        styleChange560(toggle);
+    } else if (window.innerWidth <= 480 && window.innerWidth > 450) {
         styleChange480(toggle);
+    } else if (window.innerWidth <= 450 && window.innerWidth > 375) {
+        styleChange450(toggle);
     } else if (window.innerWidth <= 375 && window.innerWidth > 320) {
         styleChange375(toggle);
     } else if (window.innerWidth <= 320) {
@@ -45,19 +51,43 @@ export function sizeMenu() {
 function styleChange768(language) {
     if (language) {
         menuLink.style.top = '63px';
-        menuLink.style.left = '182px';
+        menuLink.style.left = '178px';
         menuLink.style.padding = '18px 0.5px';
 
         menuSocial.style.top = '130px';
-        menuSocial.style.left = '182px';
-        menuSocial.style.padding = '0 47.5px';
+        menuSocial.style.left = '178px';
+        menuSocial.style.padding = '0 51px';
     } else {
-        menuLink.style.left = '155px';
+        menuLink.style.left = '172px';
         menuLink.style.padding = '18px 0';
 
         menuSocial.style.top = '130px';
-        menuSocial.style.left = '155px';
-        menuSocial.style.padding = '0 73px';
+        menuSocial.style.left = '172px';
+        menuSocial.style.padding = '0 60px';
+    }
+}
+
+/**
+ * Определение стилей при изменении языка
+ * на маленьком расширении
+ * @param {bool} language 
+ */
+function styleChange725(language) {
+    if (language) {
+        menuLink.style.top = '63px';
+        menuLink.style.left = '125px';
+        menuLink.style.padding = '18px 0.5px';
+
+        menuSocial.style.top = '130px';
+        menuSocial.style.left = '125px';
+        menuSocial.style.padding = '0 51px';
+    } else {
+        menuLink.style.left = '112px';
+        menuLink.style.padding = '18px 0';
+
+        menuSocial.style.top = '130px';
+        menuSocial.style.left = '112px';
+        menuSocial.style.padding = '0 60px';
     }
 }
 
@@ -71,11 +101,31 @@ function styleChange600(language) {
         menuLink.style.left = '100px';
         menuLink.style.padding = '18px 1px';
         menuSocial.style.left = '100px';
-        menuSocial.style.padding = '0 48.5px';
+        menuSocial.style.top = '131px';
+        menuSocial.style.padding = '0 52px';
     } else {
-        menuLink.style.left = '65px';
-        menuSocial.style.left = '65px';
-        menuSocial.style.padding = '0 74.5px';
+        menuLink.style.left = '80px';
+        menuSocial.style.left = '80px';
+        menuSocial.style.padding = '0 61.5px';
+    }
+}
+
+/**
+ * Определение стилей при изменении языка
+ * на маленьком расширении
+ * @param {bool} language 
+ */
+function styleChange560(language) {
+    if (language) {
+        menuLink.style.left = '49px';
+        menuLink.style.padding = '18px 1px';
+        menuSocial.style.left = '49px';
+        menuSocial.style.top = '129px';
+        menuSocial.style.padding = '0 52px';
+    } else {
+        menuLink.style.left = '40px';
+        menuSocial.style.left = '40px';
+        menuSocial.style.padding = '0 61.5px';
     }
 }
 
@@ -90,12 +140,32 @@ function styleChange480(language) {
         menuLink.style.padding = '18px 1px';
         menuLink.classList.add('menu480');
         menuSocial.style.left = '40px';
-        menuSocial.style.padding = '0 48.5px';
+        menuSocial.style.padding = '0 52px';
     } else {
-        menuLink.style.left = '10px';
+        menuLink.style.left = '30px';
         menuLink.classList.add('menu480');
-        menuSocial.style.left = '10px';
-        menuSocial.style.padding = '0 74.5px';
+        menuSocial.style.left = '30px';
+        menuSocial.style.padding = '0 61.5px';
+    }
+}
+
+/**
+ * Определение стилей при изменении языка
+ * на маленьком расширении
+ * @param {bool} language 
+ */
+function styleChange450(language) {
+    if (language) {
+        menuLink.style.left = '0';
+        menuLink.style.padding = '18px 1px';
+        menuLink.classList.add('menu480');
+        menuSocial.style.left = '0';
+        menuSocial.style.padding = '0 50px';
+    } else {
+        menuLink.style.left = '0';
+        menuLink.classList.add('menu480');
+        menuSocial.style.left = '0';
+        menuSocial.style.padding = '0 49.5px';
     }
 }
 
@@ -107,20 +177,20 @@ function styleChange480(language) {
 function styleChange375(language) {
     if (language) {
         menuLink.style.top = '51px';
-        menuLink.style.left = '2px';
+        menuLink.style.left = '0';
         menuLink.style.padding = '18px 20px';
         menuLink.classList.add('menu480');
-        menuSocial.style.left = '2px';
-        menuSocial.style.top = '119px';
-        menuSocial.style.padding = '5px 36px';
+        menuSocial.style.left = '0';
+        menuSocial.style.top = '135px';
+        menuSocial.style.padding = '5px 37.5px';
     } else {
         menuLink.style.top = '51px';
-        menuLink.style.left = '1px';
+        menuLink.style.left = '0';
         menuLink.style.padding = '18px 0';
         menuLink.classList.add('menu480');
-        menuSocial.style.top = '119px';
-        menuSocial.style.left = '1px';
-        menuSocial.style.padding = '0 35.5px';
+        menuSocial.style.top = '135px';
+        menuSocial.style.left = '0';
+        menuSocial.style.padding = '0 37.5px';
     }
 }
 
@@ -140,11 +210,11 @@ function styleChange320(language) {
         menuSocial.style.padding = '5px 12.5px';
     } else {
         menuLink.style.top = '51px';
-        menuLink.style.left = '0';
+        menuLink.style.left = '-6px';
         menuLink.style.padding = '9px 0';
         menuLink.classList.add('menu480');
         menuSocial.style.top = '119px';
-        menuSocial.style.left = '0';
-        menuSocial.style.padding = '0 12.5px';
+        menuSocial.style.left = '-6px';
+        menuSocial.style.padding = '0 17.5px';
     }
 }
