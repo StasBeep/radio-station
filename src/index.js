0349c41ce2abeb0f10b052ffec552558a2a5e0b1
Stas Osk <73402405+StasBeep@users.noreply.github.com>
2022-12-15 01:36:48 +0300

    Наброски для правильной работы слайдера с подключением API 1. Более плавная стилизация блоков

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 "use strict";
+import './_slider.js';
+
+let content  = document.querySelectorAll('.slider-figure-li-title')[3];
+
+console.log(content.innerText.length);
+
+// ! 265 символов текста без ...
