152454038cabc4cabb2cde4c0990dfe47b133ae1
StasBeep <stas-stas.stanislav@mail.ru>
2023-07-04 16:35:54 +0300

    Слайдер в отдельный блок

---
+++ src/components/SliderBlock.vue
@@ -0,0 +1,58 @@
+<template>
+    <ul class="slider-figure">
+        <li class="slider-figure-li">
+            <div class="slider-figure-li-wrapper">
+                <span class="slider-figure-li-title">
+                    интервью с регги-ветераном, который на ямайке был популярнее боба марли
+                </span>
+                <span class="slider-figure-li-date">
+                    06.12.2021
+                </span>
+            </div>
+            <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
+            <button class="slider-figure-li-right">&#10095;</button>
+            <button class="slider-figure-li-left">&#10094;</button>
+        </li>
+        <li class="slider-figure-li">
+            <div class="slider-figure-li-wrapper">
+                <span class="slider-figure-li-title">
+                    интервью с регги-ветераном, который на ямайке был популярнее боба марли
+                </span>
+                <span class="slider-figure-li-date">
+                    06.12.2021
+                </span>
+            </div>
+            <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
+        </li>
+        <li class="slider-figure-li">
+            <div class="slider-figure-li-wrapper">
+                <span class="slider-figure-li-title">
+                    интервью с регги-ветераном, который на ямайке был популярнее боба марли
+                </span>
+                <span class="slider-figure-li-date">
+                    06.12.2021
+                </span>
+            </div>
+            <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
+        </li>
+        <li class="slider-figure-li">
+            <div class="slider-figure-li-wrapper">
+                <span class="slider-figure-li-title">
+                    интервью с регги-ветераном, который на ямайке был популярнее боба марли
+                </span>
+                <span class="slider-figure-li-date">
+                    06.12.2021
+                </span>
+            </div>
+            <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name: 'SliderBlock'
+}
+</script>
+
+<style src="../scss/style.scss" lang="scss" scoped/>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -100,54 +100,6 @@
           <h2 class="slider-title">
             регги блог
           </h2>
-          <ul class="slider-figure">
-            <li class="slider-figure-li">
-              <div class="slider-figure-li-wrapper">
-                <span class="slider-figure-li-title">
-                  интервью с регги-ветераном, который на ямайке был популярнее боба марли
-                </span>
-                <span class="slider-figure-li-date">
-                  06.12.2021
-                </span>
-              </div>
-              <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
-              <button class="slider-figure-li-right">&#10095;</button>
-              <button class="slider-figure-li-left">&#10094;</button>
-            </li>
-            <li class="slider-figure-li">
-              <div class="slider-figure-li-wrapper">
-                <span class="slider-figure-li-title">
-                  интервью с регги-ветераном, который на ямайке был популярнее боба марли
-                </span>
-                <span class="slider-figure-li-date">
-                  06.12.2021
-                </span>
-              </div>
-              <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
-            </li>
-            <li class="slider-figure-li">
-              <div class="slider-figure-li-wrapper">
-                <span class="slider-figure-li-title">
-                  интервью с регги-ветераном, который на ямайке был популярнее боба марли
-                </span>
-                <span class="slider-figure-li-date">
-                  06.12.2021
-                </span>
-              </div>
-              <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
-            </li>
-            <li class="slider-figure-li">
-              <div class="slider-figure-li-wrapper">
-                <span class="slider-figure-li-title">
-                  интервью с регги-ветераном, который на ямайке был популярнее боба марли
-                </span>
-                <span class="slider-figure-li-date">
-                  06.12.2021
-                </span>
-              </div>
-              <a class="slider-figure-li-link" href="#" target="_blank" rel="nofollow">читать</a>
-            </li>
-          </ul>
         </article>
         <!--Четвёртая секция-->
         <article class="ether" id="ether">
